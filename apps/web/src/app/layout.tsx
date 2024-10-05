1991c09a511f1af9e84c8c768958b204fa3a1798
zeSchlausKwab <schlauskwab@proton.me>
2024-10-05 08:47:03 +0200

    feat: no dist in db

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,19 +1,19 @@
-import { Toaster } from "@/components/ui/toaster";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Toaster } from '@/components/ui/toaster'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Earthly Land",
-  description: "A social geojson editor built on nostr",
-};
+  title: 'Earthly Land',
+  description: 'A social geojson editor built on nostr',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -24,5 +24,5 @@ export default function RootLayout({
         </div>
       </body>
     </html>
-  );
+  )
 }
